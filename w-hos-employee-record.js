1a399f593186d96e8cfbc218676e1f25fc3660f1
Francis Solis <francissolis.official@gmail.com>
2026-01-16 14:13:46 +0800

    Complete the employee form design, will continue to develop radio button function.
    Pending: submit & reset button, form PHP saving.

--- w-hos-employee-record.js
+++ w-hos-employee-record.js
@@ -6,24 +6,3 @@ toggleButton.addEventListener("click", () => {
   toggleButtonBg.classList.toggle("enable");
   slideCard.classList.toggle("enable");
 });
-
-const radioButtons = document.querySelectorAll(".input-box-radio-sex");
-
-radioButtons.forEach((radioButton) => {
-  radioButton.addEventListener("click", (e) => {
-    const button = e.currentTarget; // always the .input-box-radio-sex div
-
-    if (button.id === "user-sex-female") {
-      const currentColor = getComputedStyle(button).backgroundColor;
-
-      if (currentColor === "rgb(70, 130, 180)") { // steelblue
-        button.style.backgroundColor = "white";
-      } else {
-        button.style.backgroundColor = "steelblue";
-      }
-    } else {
-      console.log("male");
-    }
-  });
-});
-
